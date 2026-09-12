0d8727a9acf70c716a0a9b7f91ad9768bef7649d
Emad <emadmarey29@gmail.com>
2026-09-12 23:06:05 +0300

    first commit

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -5,4 +5,8 @@ class AppColors {
 
   static const Color primary = Color(0xFF1B5E4F);
   static const Color primaryDark = Color(0xFF103D33);
+  static const Color deepForest = Color(0xFF003527);
+  static const Color champagneGold = Color(0xFFE6C778);
+  static const Color gold = Color(0xFFD4AF37);
+  static const Color luminousIvory = Color(0xFFF7FBF9);
 }
