53b4f30fee367ee2971c10af708932d7b128bf0c
Emad <emadmarey29@gmail.com>
2026-09-18 20:12:12 +0300

    first commit

---
+++ lib/core/theme/app_sizes.dart
@@ -0,0 +1,22 @@
+class AppSizes {
+  AppSizes._();
+
+  // Centralized typography font sizes
+  static const double sp10 = 10.0;
+  static const double sp11 = 11.0;
+  static const double sp12 = 12.0;
+  static const double sp13 = 13.0;
+  static const double sp14 = 14.0;
+  static const double sp15 = 15.0;
+  static const double sp16 = 16.0;
+  static const double sp17 = 17.0;
+  static const double sp18 = 18.0;
+  static const double sp20 = 20.0;
+  static const double sp22 = 22.0;
+  static const double sp24 = 24.0;
+  static const double sp26 = 26.0;
+  static const double sp28 = 28.0;
+  static const double sp32 = 32.0;
+  static const double sp48 = 48.0;
+  static const double sp58 = 58.0;
+}
