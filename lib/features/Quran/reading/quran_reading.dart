20a7868116453dd4a0d22753ec8fc6b62b0b38bc
Emad <emadmarey29@gmail.com>
2026-09-19 23:04:46 +0300

    first commit

--- lib/features/Quran/reading/quran_reading.dart
+++ lib/features/Quran/reading/quran_reading.dart
@@ -2,9 +2,9 @@ import 'package:deenora/features/Quran/controller/quran_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../../../core/skeleton/quran_skeleton_screen.dart';
-import '../../../core/theme/app_colors.dart';
 import '../../../core/widget/error/error_screen.dart';
 import '../../../core/widget/share_widget/future_builder_share.dart';
+import '../../../core/widget/share_widget/surah_list_item_card.dart';
 import 'details/surah_details_screen.dart';
 import 'models/surah_model.dart';
 
@@ -35,120 +35,17 @@ class QuranReading extends StatelessWidget {
                   itemBuilder: (context, index) {
                     final surah = quran[index];
 
-                    return Container(
-                      margin: const EdgeInsets.only(bottom: 10),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(27),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black.withValues(alpha: 0.12),
-                            blurRadius: 8,
-                            offset: const Offset(0, 3),
+                    return SurahListItemCard(
+                      surah: surah,
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                SurahDetailsScreen(surah: surah),
                           ),
-                        ],
-                      ),
-                      child: Material(
-                        color: Colors.transparent,
-                        borderRadius: BorderRadius.circular(27),
-                        child: InkWell(
-                          borderRadius: BorderRadius.circular(27),
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) =>
-                                    SurahDetailsScreen(surah: surah),
-                              ),
-                            );
-                          },
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(
-                              horizontal: 14,
-                              vertical: 12,
-                            ),
-                            child: Row(
-                              children: [
-                                Container(
-                                  width: 44,
-                                  height: 44,
-                                  decoration: BoxDecoration(
-                                    color: AppColors.primary.withValues(
-                                      alpha: 0.1,
-                                    ),
-                                    shape: BoxShape.circle,
-                                  ),
-                                  child: Center(
-                                    child: Text(
-                                      '${surah.number}',
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .titleMedium
-                                          ?.copyWith(color: AppColors.primary),
-                                    ),
-                                  ),
-                                ),
-
-                                const SizedBox(width: 15),
-
-                                Expanded(
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        surah.englishName,
-                                        style: Theme.of(
-                                          context,
-                                        ).textTheme.titleLarge,
-                                      ),
-                                      const SizedBox(height: 4),
-                                      Text(
-                                        '${surah.ayahs.length} verses',
-                                        style: Theme.of(
-                                          context,
-                                        ).textTheme.bodySmall,
-                                      ),
-                                    ],
-                                  ),
-                                ),
-
-                                Container(
-                                  padding: const EdgeInsets.symmetric(
-                                    horizontal: 8,
-                                    vertical: 4,
-                                  ),
-                                  decoration: BoxDecoration(
-                                    color: surah.revelationType == 'Meccan'
-                                        ? Colors.orange.withValues(alpha: 0.12)
-                                        : Colors.blue.withValues(alpha: 0.12),
-                                    borderRadius: BorderRadius.circular(10),
-                                  ),
-                                  child: Text(
-                                    surah.revelationType,
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .labelSmall
-                                        ?.copyWith(
-                                          color:
-                                              surah.revelationType == 'Meccan'
-                                              ? Colors.orange[900]
-                                              : Colors.blue[900],
-                                        ),
-                                  ),
-                                ),
-
-                                const SizedBox(width: 6),
-
-                                Icon(
-                                  Icons.chevron_right,
-                                  color: Colors.grey[400],
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
+                        );
+                      },
                     );
                   },
                 );
